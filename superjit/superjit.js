301aea792da005f08b7a154385f5d9c81132cad7
Christian Dorato <christian.a.dorato@gmail.com>
2024-08-16 19:18:08 -0400

    Actually made it work

--- superjit/superjit.js
+++ superjit/superjit.js
@@ -1,5 +1,7 @@
-tracks = {"track":1, "name":"song1", "length":"3:03", "file":"lcb_WhoDeniesAll_OST", "extension":".mp3"}
+tracks = {"track":1, "name":"Canto IV: Who Denies All", "length":"3:03", "file":"lcb_WhoDeniesAll_OST", "extension":".mp3"}
 mediaPath = "audio/";
 var audio = document.getElementById("cool");
 audio.src = mediaPath + tracks.file + tracks.extension;
 console.log(mediaPath + tracks.file + tracks.extension);
+hi = document.getElementById("cool2");
+hi.innerHTML = "Now playing: " + "<b>" + tracks.name + "</b>";
